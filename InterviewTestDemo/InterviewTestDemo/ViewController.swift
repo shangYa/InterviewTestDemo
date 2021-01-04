fc2e0bcf7daa19212e2118de976a69cf4cb55ea5
zhengxx <316059126@qq.com>
2021-01-04 09:55:53 +0800

    创建空项目

---
+++ InterviewTestDemo/InterviewTestDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterviewTestDemo
+//
+//  Created by NewApple on 2021/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
